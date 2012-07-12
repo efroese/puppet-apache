844e38374e15d41ee68c322c5489cf5922a3b208
Erik Froese <erik.froese@gmail.com>
2012-07-12 08:52:29 -0400

    Clean up the Amazon support.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,45 +1,45 @@
 class apache::params {
 
   $pkg = $::operatingsystem ? {
-    /RedHat|CentOS|Amazon|Linux/ => 'httpd',
+    /RedHat|CentOS|Amazon/ => 'httpd',
     /Debian|Ubuntu/ => 'apache2',
   }
 
   $root = $apache_root ? {
     "" => $::operatingsystem ? {
-      /RedHat|CentOS|Amazon|Linux/ => '/var/www/vhosts',
+      /RedHat|CentOS|Amazon/ => '/var/www/vhosts',
       /Debian|Ubuntu/ => '/var/www',
     },
     default => $apache_root
   }
 
   $user = $::operatingsystem ? {
-    /RedHat|CentOS|Amazon|Linux/ => 'apache',
+    /RedHat|CentOS|Amazon/ => 'apache',
     /Debian|Ubuntu/ => 'www-data',
   }
 
   $group = $operatingsystem ? {
-    /RedHat|CentOS/ => 'apache',
+    /RedHat|CentOS|Amazon/ => 'apache',
     /Debian|Ubuntu/ => 'www-data',
   }
 
   $conf = $operatingsystem ? {
-    /RedHat|CentOS/ => '/etc/httpd',
+    /RedHat|CentOS|Amazon/ => '/etc/httpd',
     /Debian|Ubuntu/ => '/etc/apache2',
   }
 
   $log = $::operatingsystem ? {
-    /RedHat|CentOS|Amazon|Linux/ => '/var/log/httpd',
+    /RedHat|CentOS|Amazon/ => '/var/log/httpd',
     /Debian|Ubuntu/ => '/var/log/apache2',
   }
 
   $access_log = $::operatingsystem ? {
-    /RedHat|CentOS|Amazon|Linux/ => "${log}/access_log",
+    /RedHat|CentOS|Amazon/ => "${log}/access_log",
     /Debian|Ubuntu/ => "${log}/access.log",
   }
 
   $error_log = $::operatingsystem ? {
-    /RedHat|CentOS|Amazon|Linux/ => "${log}/error_log",
+    /RedHat|CentOS|Amazon/ => "${log}/error_log",
     /Debian|Ubuntu/ => "${log}/error.log",
   }
 
